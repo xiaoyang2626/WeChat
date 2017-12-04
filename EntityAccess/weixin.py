16bb2f5903a5a76fe39552507b390ba092a05290
xiaoyang2626 <yanghaocheng_2008@163.com>
2017-12-04 13:27:16 +0800

    Add files via upload

---
+++ EntityAccess/weixin.py
@@ -0,0 +1,95 @@
+#coding:utf-8
+import re
+import time
+import json
+import hashlib
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(clientip)s %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='weixin.log',
+                    filemode='a')
+import pinyin
+import xmltodict
+class sign(object):
+    #服务器信息验证接收信息[signature,timestamp,nonce,echostr]
+    def authtoken(self,signature,timestamp,nonce,echostr):
+        try:
+            data = ['15900253421xiaoyangYHC', timestamp, nonce]
+            data.sort()
+            sha1 = hashlib.sha1()
+            for item in data:
+                sha1.update(item.encode('utf-8'))
+            hashcode = sha1.hexdigest()
+            if hashcode == signature:
+                return echostr
+            else:
+                return ''
+        except Exception as ex:
+            logging.error(str(ex))
+class message(object):
+    '消息处理'
+    def __init__(self,xml):
+        self.__dicdata = json.loads(json.dumps(xmltodict.parse(xml)))
+        self.__ToUserName = self.__dicdata['xml']['ToUserName']
+        self.__FromUserName = self.__dicdata['xml']['FromUserName']
+        self.__CreateTime = self.__dicdata['xml']['CreateTime']
+        self.__MsgType = self.__dicdata['xml']['MsgType']
+        self.__MsgId = self.__dicdata['xml']['MsgId']
+    def receive(self):
+        if self.__MsgType=='text':
+            self.__Content=self.__dicdata['xml']['Content']
+        elif self.__MsgType=='voice':
+            self.__Recognition=self.__dicdata['xml']['Recognition']
+            
+    def answer(self):
+        try:
+            if self.__MsgType == 'text':
+                print('text:' + self.__Content)
+                if re.search('廖英强', self.__Content):
+                    resultxml = {'xml': {
+                        'ToUserName': self.__FromUserName,
+                        'FromUserName': self.__ToUserName,
+                        'CreateTime': str(int(round(time.time() * 1000))),
+                        'MsgType': 'news',
+                        'ArticleCount': 1,
+                        'Articles': {
+                            'item': {
+                                'Title': '廖英强解盘',
+                                'Description': '廖英强解盘',
+                                'PicUrl': 'http://www.zhangwocj.com/static/images/video_sort_01.png',
+                                'Url': 'http://www.zhangwocj.com/video1?uid=599172f178a8051cd2b111a4&key=111&live_id=598c213aa2fea4f51978e0de&title=%E5%BB%96%E8%8B%B1%E5%BC%BA%E8%80%81%E5%B8%88%E6%AF%8F%E6%97%A5%E8%A7%A3%E7%9B%98'
+                            }
+                        }
+                    }}
+                    return xmltodict.unparse(resultxml)
+            
+            
+        except Exception as ex:
+            logging.error(str(ex))
+        
+        try:
+            if self.__MsgType == 'voice':
+                print('voice:' + self.__Recognition)
+                if re.search('liaoyingqiang', pinyin.get(self.__Recognition, format="strip", delimiter='')):
+                    result = {'xml': {
+                        'ToUserName': self.__FromUserName,
+                        'FromUserName': self.__ToUserName,
+                        'CreateTime': str(int(round(time.time() * 1000))),
+                        'MsgType': 'news',
+                        'ArticleCount': 1,
+                        'Articles': {
+                            'item': {
+                                'Title': '廖英强解盘',
+                                'Description': '廖英强解盘',
+                                'PicUrl': 'http://www.zhangwocj.com/static/images/video_sort_01.png',
+                                'Url': 'http://www.zhangwocj.com/video1?uid=599172f178a8051cd2b111a4&key=111&live_id=598c213aa2fea4f51978e0de&title=%E5%BB%96%E8%8B%B1%E5%BC%BA%E8%80%81%E5%B8%88%E6%AF%8F%E6%97%A5%E8%A7%A3%E7%9B%98'
+                            }
+                        }
+                    }}
+            
+                    return xmltodict.unparse(result)
+        except Exception as ex:
+            logging.error(str(ex))
+        
+        return 'success'
---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import time
+import json
+import logging;
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(clientip)s %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='app.log',
+                    filemode='a')
+
+import EntityAccess.weixin
+
+import xmltodict
+from flask import Flask, request, render_template, redirect, url_for, escape, session, jsonify
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+monkey.patch_all()
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.secret_key = os.urandom(12)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('helloword.html')
+
+
+@app.route('/wechat', methods=['POST'])
+def GetMessage():
+    # dictdata=json.loads(json.dumps(xmltodict.parse(request.data)))
+    # # dictdata=json.loads(json.dumps(xmltodict.parse(request.form['body'])))
+    # print(dictdata['xml']['ToUserName'])
+    # print(dictdata['xml']['FromUserName'])
+    # print(dictdata['xml']['CreateTime'])
+    # print(dictdata['xml']['MsgType'])
+    # print(dictdata['xml']['Content'])
+    # print(dictdata['xml']['MsgId'])
+    # result = {'xml': {'ToUserName': dictdata['xml']['FromUserName'], 'FromUserName': dictdata['xml']['ToUserName'], 'CreateTime': str(
+    #     int(round(time.time() * 1000))), 'MsgType': 'text', 'Content': 'Hello World'}}
+    # print(xmltodict.unparse(result))
+    msg=EntityAccess.weixin.message(request.data)
+    msg.receive()
+    return msg.answer()
+@app.route('/wechat', methods=['GET'])
+def auth():
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
+    wxsign = EntityAccess.weixin.sign()
+    result = wxsign.authtoken(str(signature), str(timestamp), str(nonce), str(echostr))
+    print('result'+str(result))
+
+    # return render_template('weixinsign.html',signature=signature)
+    return result
+
+if __name__ == '__main__':
+    http_server=WSGIServer(('0.0.0.0', 8080), app)
+    print('yiqidong')
+    http_server.serve_forever()
